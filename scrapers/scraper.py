0ece42d1b36fe609c7ebd7a8fde7ed50495e5ff2
SakshiPanhalkar <126937522+SakshiPanhalkar@users.noreply.github.com>
2024-01-30 10:43:26 +0530

    Update doc strings and comments in :scraper.py

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,12 +1,27 @@
+# import necessary libraries
 import abc
 
-
 class Scraper(abc.ABC):
-    """abstract class
+    """
+    Abstract class
 
     Args:
         abc (self, company_name: str, keyword: str, page_count: int): scraping code
     """
+    @abc.abstractmethod
     def scrape(self, company_name: str, keyword: str, page_count: int):
+        """
+        Abstract method to be implemented by subclasses for scraping data.
+
+        Args:
+            company_name (str): Name of the company.
+            keyword (str): Extra word to be searched along with the company name.
+            page_count (int): Number of pages to be searched.
+            base_url (str): Base URL for constructing search requests.
+
+        Returns:
+            pd.DataFrame: Data scraped from the specified source.
+        """
+
 
         pass

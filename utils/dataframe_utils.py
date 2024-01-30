a9255bc660dc78314e0964bfc07a0b7455b8998c
SakshiPanhalkar <126937522+SakshiPanhalkar@users.noreply.github.com>
2024-01-30 08:18:18 +0530

    Added doc string to:dataframe_utils.py

--- utils/dataframe_utils.py
+++ utils/dataframe_utils.py
@@ -1,5 +1,14 @@
+# import necessary libraries
 import pandas as pd
 
-
 def convert_df_to_csv(df: pd.DataFrame):
+    """
+    Converts a DataFrame to a CSV file.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to be converted.
+
+    Returns:
+        None: The CSV file is saved with the name 'news.csv'.
+    """
     df.to_csv('news.csv')

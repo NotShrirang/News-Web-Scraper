0662f51ab962fb0582c6c274cf2d78edda7b0a1c
SakshiPanhalkar <126937522+SakshiPanhalkar@users.noreply.github.com>
2024-01-30 08:20:14 +0530

    Added doc string to: date_utils.py

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -1,5 +1,14 @@
+# import necessary libraries
 import dateparser
 
-
 def format_timestamp(timestamp: str):
+    """
+    Formats a timestamp string into a datetime object using dateparser.
+
+    Args:
+        timestamp (str): The timestamp string to be formatted.
+
+    Returns:
+        datetime.datetime: The formatted datetime object.
+    """
     return dateparser.parse(str(timestamp))
